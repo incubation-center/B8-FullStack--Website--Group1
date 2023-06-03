380e4be16beb61559ef65d2c6f5687fd6c7e3496
DoungChan <sreoun.doungchan19@kit.edu.kh>
2023-06-04 00:21:16 +0700

    fixed option

--- components/navbar/CategoriseOption.jsx
+++ components/navbar/CategoriseOption.jsx
@@ -1,96 +1,110 @@
-import React from "react";
-
+import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import arrowdown from "public/arrow-down.svg";
 function CategoriseOption() {
+  const [openCategory, setOpenCategory] = useState(false);
+  const ref = useRef();
+  useEffect(() => {
+    // Function to handle clicks outside the component
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setOpenCategory(false);
+      }
+    }
+
+    // Attach the event listener to the document
+    document.addEventListener("click", handleClickOutside);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
   return (
-    <div class="relative inline-block text-left">
-      <div>
+    <div ref={ref} className="relative inline-block text-left">
+      <div className="relative flex items-center bg-transparent">
         <button
           type="button"
-          class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          className="inline-flex w-full h-[27px] justify-center items-center gap-x-1.5 rounded-md bg-transparent px-4 text-sm font-semibold text-sub_font_color active:ring-primary duration-300"
           id="menu-button"
-          aria-expanded="true"
-          aria-haspopup="true"
+          // set open
+          onClick={() => setOpenCategory((prev) => !prev)}
         >
           Categories
-          <svg
-            class="-mr-1 h-5 w-5 text-gray-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="false"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          {!openCategory ? (
+            <Image src={arrowdown} className="w-4 h-4" alt="arrowdown" />
+          ) : (
+            <Image src={arrowdown} className="w-4 h-4" alt="arrowdown" />
+          )}
         </button>
       </div>
 
-      <div
-        class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-        role="menu"
-        aria-orientation="vertical"
-        aria-labelledby="menu-button"
-        tabindex="-1"
-      >
-        <div class="py-1" role="none">
-          <a
-            href="#"
-            class="text-gray-700 block px-4 py-2 text-sm"
-            role="menuitem"
-            tabindex="-1"
-            id="menu-item-0"
-          >
-            Travel
-          </a>
-          <a
-            href="#"
-            class="text-gray-700 block px-4 py-2 text-sm"
-            role="menuitem"
-            tabindex="-1"
-            id="menu-item-1"
-          >
-            Food
-          </a>
-          <a
-            href="#"
-            class="text-gray-700 block px-4 py-2 text-sm"
-            role="menuitem"
-            tabindex="-1"
-            id="menu-item-2"
-          >
-            Tech
-          </a>
-          <a
-            href="#"
-            class="text-gray-700 block px-4 py-2 text-sm"
-            role="menuitem"
-            tabindex="-1"
-            id="menu-item-2"
-          >
-            Fashion
-          </a>
-          <a
-            href="#"
-            class="text-gray-700 block px-4 py-2 text-sm"
-            role="menuitem"
-            tabindex="-1"
-            id="menu-item-2"
-          >
-            Automoative
-          </a>
-          <a
-            href="#"
-            class="text-gray-700 block px-4 py-2 text-sm"
-            role="menuitem"
-            tabindex="-1"
-            id="menu-item-2"
-          >
-            Grocery
-          </a>
+      {openCategory === true && (
+        <div
+          className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="menu-button"
+          tabindex="-1"
+        >
+          <div className="py-1" role="none">
+            <a
+              href="#"
+              className="text-gray-700 block px-4 py-2 text-sm"
+              role="menuitem"
+              tabindex="-1"
+              id="menu-item-0"
+            >
+              Travel
+            </a>
+            <a
+              href="#"
+              className="text-gray-700 block px-4 py-2 text-sm"
+              role="menuitem"
+              tabindex="-1"
+              id="menu-item-1"
+            >
+              Food
+            </a>
+            <a
+              href="#"
+              className="text-gray-700 block px-4 py-2 text-sm"
+              role="menuitem"
+              tabindex="-1"
+              id="menu-item-2"
+            >
+              Tech
+            </a>
+            <a
+              href="#"
+              className="text-gray-700 block px-4 py-2 text-sm"
+              role="menuitem"
+              tabindex="-1"
+              id="menu-item-2"
+            >
+              Fashion
+            </a>
+            <a
+              href="#"
+              className="text-gray-700 block px-4 py-2 text-sm"
+              role="menuitem"
+              tabindex="-1"
+              id="menu-item-2"
+            >
+              Automoative
+            </a>
+            <a
+              href="#"
+              className="text-gray-700 block px-4 py-2 text-sm"
+              role="menuitem"
+              tabindex="-1"
+              id="menu-item-2"
+            >
+              Grocery
+            </a>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
