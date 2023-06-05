0d5d20b6e67dc6926db125039a16f1e6d2a86aba
sivengo <sivengorn@gmail.com>
2023-06-05 23:08:15 +0700

    category/feature-promotion

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import UnauthorizeComponent from "@/components/unauthorized/UnauthorizeComponent";
+import Category from "../components/category-feature/Categories"
+import HotPromotion from "../components/category-feature/Feature"
+import Feature from "../components/category-feature/Feature";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between `}
-    >
-
+    <main className={`flex min-h-screen flex-col items-center justify-between `}>
+      <Feature />
+      <Category />
     </main>
   );
 }
