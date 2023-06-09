dd6f2372906bf1942727350da4f8471027d1f0fa
sivengo <sivengorn@gmail.com>
2023-06-09 10:37:09 +0700

    add hover to hottest promotion

--- components/category-feature/Feature.jsx
+++ components/category-feature/Feature.jsx
@@ -6,20 +6,23 @@ import timeIcon from "public/icon/time.svg";
 
 export default function Feature() {
   return (
-    <div className="mt-20 text-font_color">
+    <div className="mt-28 text-font_color">
       <h2 className="font-bold text-2xl px-2 py-4 lg:text-left sm:text-center">
         Hey, check out today&#39;s hottest promotions
       </h2>
 
       <div className="flex flex-row flex-wrap justify-center items-center">
         <div className="p-2">
-          <Image
-            src={orange}
-            width={418}
-            height={232}
-            alt="image"
-            className="rounded-md"
-          />
+          <div className="relative max-w-md overflow-hidden bg-cover bg-no-repeat rounded-md">
+            <Image
+              src={orange}
+              className="max-w-md transition duration-300 ease-in-out hover:scale-110"
+              width={418}
+              height={232}
+              alt="image"
+            />
+          </div>
+
           <div className="flex flex-col flex-wrap pt-3">
             <div className="flex flex-row">
               <Image src={timeIcon} width={23} height={23} alt="timeIcon" />
@@ -27,7 +30,7 @@ export default function Feature() {
             </div>
             <div className="text-lg font-bold">ABC Restaurant Drink</div>
             <div className="text-base">Street 704, Phnom Penh</div>
-            <div className="flex flex-row pt-1">
+            <div className="flex flex-row pt-1 items-center">
               <div className="flex flex-row text-xl">
                 <div className="line-through">$10</div>
                 <div className="font-bold px-1">$5</div>
@@ -41,13 +44,15 @@ export default function Feature() {
           </div>
         </div>
         <div className="p-2">
-          <Image
-            src={drink}
-            width={418}
-            height={232}
-            alt="image"
-            className="rounded-md"
-          />
+          <div className="relative max-w-md overflow-hidden bg-cover bg-no-repeat rounded-md">
+            <Image
+              src={drink}
+              className="max-w-md transition duration-300 ease-in-out hover:scale-110"
+              width={418}
+              height={232}
+              alt="image"
+            />
+          </div>
           <div className="flex flex-col flex-wrap pt-3">
             <div className="flex flex-row">
               <Image src={timeIcon} width={23} height={23} alt="timeIcon" />
@@ -55,7 +60,7 @@ export default function Feature() {
             </div>
             <div className="text-lg font-bold">ABC Restaurant Drink</div>
             <div className="text-base">Street 704, Phnom Penh</div>
-            <div className="flex flex-row pt-1">
+            <div className="flex flex-row pt-1 items-center">
               <div className="flex flex-row text-xl">
                 <div className="line-through">$10</div>
                 <div className="font-bold px-1">$5</div>
@@ -69,13 +74,15 @@ export default function Feature() {
           </div>
         </div>
         <div className="p-2">
-          <Image
-            src={smoothie}
-            width={418}
-            height={232}
-            alt="image"
-            className="rounded-md"
-          />
+          <div className="relative max-w-md overflow-hidden bg-cover bg-no-repeat rounded-md">
+            <Image
+              src={smoothie}
+              className="max-w-md transition duration-300 ease-in-out hover:scale-110"
+              width={418}
+              height={232}
+              alt="image"
+            />
+          </div>
           <div className="flex flex-col flex-wrap pt-3">
             <div className="flex flex-row">
               <Image src={timeIcon} width={23} height={23} alt="timeIcon" />
@@ -83,7 +90,7 @@ export default function Feature() {
             </div>
             <div className="text-lg font-bold">ABC Restaurant Drink</div>
             <div className="text-base">Street 704, Phnom Penh</div>
-            <div className="flex flex-row pt-1">
+            <div className="flex flex-row pt-1 items-center">
               <div className="flex flex-row text-xl">
                 <div className="line-through">$10</div>
                 <div className="font-bold px-1">$5</div>
