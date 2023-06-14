4019a8d6c32a305c1fdddc418822819d15c63dc1
sivengo <sivengorn@gmail.com>
2023-06-14 13:57:34 +0700

    render to 404 if cat not found

--- pages/category/[categoryId].jsx
+++ pages/category/[categoryId].jsx
@@ -1,8 +1,9 @@
+import { useEffect } from "react";
 import { Promotions } from "@/components/popular/Promotions";
-import PromotionCard from "@/components/popular/PromotionCard"
+import PromotionCard from "@/components/popular/PromotionCard";
 import { useRouter } from "next/router";
 import { Categories } from "./Categories";
-
+import { TailSpin } from "react-loader-spinner";
 
 const CategoryDetail = () => {
   const router = useRouter();
@@ -11,13 +12,18 @@ const CategoryDetail = () => {
   // Find the category based on the categoryId
   const category = Categories.find((cat) => cat.id === Number(categoryId));
 
+  useEffect(() => {
+    if (categoryId && !category) {
+      // Redirect to 404 page when category is not found
+      router.push("/404");
+    }
+  }, [categoryId, category, router]);
+
   if (!category) {
-    // Handle case when category is not found
+    // Render a loading spinner or message while redirecting to 404 page
     return (
-      <div className="flex justify-center p-24">
-        <h1 className="my-20 text-2xl font-bold text-font_color">
-          Category not found
-        </h1>
+      <div className="flex justify-center items-center p-48">
+        <TailSpin color="#00BFFF" height={80} width={80} />
       </div>
     );
   }
@@ -30,14 +36,15 @@ const CategoryDetail = () => {
   return (
     <div className="flex justify-center py-24">
       {/* Rest of the category detail page */}
-
       <div>
-        <h1 className="my-8 text-2xl font-bold text-font_color">{`${category.name}`}</h1>
+        <h1 className="my-8 text-2xl font-bold text-font_color">
+          {category.name}
+        </h1>
         <div>
           <div className="grid grid-cols-4 max-[480px]:grid-cols-1 gap-8">
-            {categoryPromotions.map((promotion, index) => {
-              return <PromotionCard promotion={promotion} key={index} />;
-            })}
+            {categoryPromotions.map((promotion, index) => (
+              <PromotionCard promotion={promotion} key={index} />
+            ))}
           </div>
         </div>
       </div>
