bfcae21dfbccd8c44d143618b6d52d04a06ea40c
Sen Rith <senrithney2@gmail.com>
2023-06-17 00:17:14 +0700

    fix: server side can't send request

--- utils/serverApiClient.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const serverApiClient = axios.create({
-  baseURL: process.env.API_URL,
-});
-
-serverApiClient.defaults.headers.common["Api-Token"] = process.env.API_TOKEN;
-
-export default serverApiClient;
