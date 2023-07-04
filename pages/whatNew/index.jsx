4ad5b0520305a77897f000681e5345b72769098a
DoungChan <sreoun.doungchan19@kit.edu.kh>
2023-07-04 23:59:36 +0700

    add component

--- pages/whatNew/index.jsx
+++ pages/whatNew/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PromotionCard from "@/components/popular/PromotionCard";
 import { Promotions } from "@/components/popular/Promotions";
 import { searchAtom } from "@/state/recoilAtoms";
@@ -7,11 +7,19 @@ import { useRouter } from "next/router";
 import CustomPagination from "@/components/pagination/CustomPagination";
 
 import Head from "next/head";
-const WhatNew = ({ data }) => {
+import { SearchNotFound } from "@/components/search-notFound/SearchNotFound";
+const WhatNew = ({ data, error }) => {
   const router = useRouter();
   const query = router.query.search || "";
 
   const searchValue = useRecoilValue(searchAtom);
+
+  // handle server error
+  useEffect(() => {
+    if (error) {
+      router.push("/500");
+    }
+  }, [error]);
 
   const title =
     query === "" ? "What's new" : `Search result for "${searchValue}"`;
@@ -21,26 +29,31 @@ const WhatNew = ({ data }) => {
         <title>What New | PromoKh</title>
         <link rel="icon" href="/logo.png" />
       </Head>
-      <div className="py-10">
-        <div className="m-10 flex justify-center">
-          <div>
-            <h1 className="my-8 text-2xl font-bold text-font_color">
-              {"What's new"}
-            </h1>
+
+      {data?.data?.length === 0 ? (
+        <SearchNotFound />
+      ) : (
+        <div className="py-10">
+          <div className="m-10 flex justify-center">
             <div>
-              <div className="grid grid-cols-4 max-[480px]:grid-cols-1 gap-8">
-                {data?.data?.map((promotion, index) => {
-                  return <PromotionCard promotion={promotion} key={index} />;
-                })}
+              <h1 className="my-8 text-2xl font-bold text-font_color">
+                {"What's new"}
+              </h1>
+              <div>
+                <div className="grid grid-cols-4 max-[480px]:grid-cols-1 gap-8">
+                  {data?.data?.map((promotion, index) => {
+                    return <PromotionCard promotion={promotion} key={index} />;
+                  })}
+                </div>
               </div>
             </div>
           </div>
+          <CustomPagination
+            resPerPage={24}
+            promotionsCount={data.totalElements}
+          />
         </div>
-        <CustomPagination
-          resPerPage={24}
-          promotionsCount={data.totalElements}
-        />
-      </div>
+      )}
     </>
   );
 };
@@ -97,7 +110,6 @@ export const getServerSideProps = async (context) => {
           notFound: true,
         };
       }
-
       return {
         props: {
           data,
