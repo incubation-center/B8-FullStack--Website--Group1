b72d1eaf6b84998dd9f67d9b8b6edbbb8721f1f4
Sen Rith <senrithney2@gmail.com>
2023-07-06 18:46:40 +0700

    :sparkles: upload file

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -14,8 +14,6 @@ export default async function handler(req, res) {
       });
 
       const data = await response.json();
-      const accessToken = data.accessToken;
-      localStorage.setItem("accessToken", accessToken);
 
       res.status(response.status).json(data);
     } catch (error) {
